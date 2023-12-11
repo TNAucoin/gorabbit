a083d68c7bd11e7fc62e5cca9b2d1cee2c7c1f2b
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-11 08:23:25 -0600

    Enhance README and optimize RabbitMQ's task distribution
    This update improves the README documentation, introducing more detailed descriptions and a graphical illustration of the message flow. A change is also committed to the RabbitMQ handler in the worker.go file, improving its task distribution efficiency by assigning tasks to idle workers when possible. A new error handling structure is implemented as well, ensuring better response under error scenarios.

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Go and RabbitMQ
 
 Exploring RabbitMQ with Go
 
+This project consists of a few different RabbitMQ programs to explore the basics ideas
+around working with producers, consumers, and queues in RabbitMQ.
+
+#### Producer / Consumer
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -8,12 +8,15 @@ import (
 	"time"
 )
 
+// main function is the entry point of the program.
 func main() {
 	rmq, err := rmq.NewRabbitMQ("task", true)
 	errors.HandleErrorWithMessage(err, "could not create rabbitmq")
 	receivedMessages, err := rmq.Consume()
 	errors.HandleErrorWithMessage(err, "could not consume messages")
-
+	// This helps spread the work around to free worker nodes, by assigning new tasks to idle workers if possible
+	err = rmq.Channel.Qos(1, 0, false)
+	errors.HandleErrorWithMessage(err, "could not configure QoS")
 	var forever chan struct{}
 	go func() {
 		for msg := range receivedMessages {
@@ -22,6 +25,7 @@ func main() {
 			t := time.Duration(dotCount)
 			time.Sleep(t * time.Second)
 			log.Printf("done")
+			msg.Ack(false)
 		}
 	}()
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C\n")
